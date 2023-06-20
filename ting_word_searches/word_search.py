989552c1d839e2bd5dc7e7419e3e5a39e0386304
Bruno Serpa <brunospenna@gmail.com>
2023-06-20 09:22:39 -0300

    req 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 from ting_file_management.queue import Queue
 
 
@@ -22,5 +23,25 @@ def exists_word(word: str, instance: Queue):
     return word_results
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word: str, instance: Queue):
+    word_results = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+        word_occurrences = []
+
+        for line_number, line_content in enumerate(file_info["linhas_do_arquivo"], start=1):
+            if word.lower() in line_content.lower():
+                word_occurrences.append({
+                    "linha": line_number,
+                    "conteudo": line_content.strip(),
+                })
+
+        if word_occurrences:
+            word_results.append({
+                "palavra": word,
+                "arquivo": file_info["nome_do_arquivo"],
+                "ocorrencias": word_occurrences,
+            })
+
+    return word_results
